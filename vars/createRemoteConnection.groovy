7e42ce965c067f0e084e0fa687b7c508d096aef5
Dmitriy Melnichenko <tairezzzz@gmail.com>
2020-09-07 13:35:29 +0300

    add sshUserPrivateKey to createRemoteConnection

--- vars/createRemoteConnection.groovy
+++ vars/createRemoteConnection.groovy
@@ -1,22 +1,43 @@
 def call(env, params) {
-    def REMOTE = [:];
+  def REMOTE = [:];
 
-    params.each { value ->
-        def condition = value.get('condition');
-        if (env in condition || env == condition) {
-            withCredentials([usernamePassword(
-                    credentialsId: value.get('credentialsId'),
-                    usernameVariable: 'USERNAME',
-                    passwordVariable: 'PASSWORD'
-            )]) {
-                REMOTE.name = value.get('host');
-                REMOTE.host = value.get('host');
-                REMOTE.user = USERNAME;
-                REMOTE.password = PASSWORD;
-                REMOTE.allowAnyHosts = true;
-            }
+  params.each { value ->
+    def condition = value.get('condition');
+    if (env in condition || env == condition) {
+
+      REMOTE.name = value.get('host');
+      REMOTE.host = value.get('host');
+      REMOTE.allowAnyHosts = true;
+
+      try {
+        withCredentials([usernamePassword(
+          credentialsId: value.get('credentialsId'),
+          usernameVariable: 'USERNAME',
+          passwordVariable: 'PASSWORD'
+        )]) {
+          REMOTE.user = USERNAME;
+          REMOTE.password = PASSWORD;
         }
-    }
+      } catch (e) {
+      }
 
-    return REMOTE;
+      try {
+        withCredentials([sshUserPrivateKey(
+          credentialsId: value.get('credentialsId'),
+          keyFileVariable: 'KEY_FILE',
+          usernameVariable: 'USERNAME',
+        )]) {
+          REMOTE.user = USERNAME;
+          REMOTE.identity = sh(
+            script: "cat $KEY_FILE",
+            returnStdout: true
+          )
+        }
+      } catch (e) {
+      }
+    }
+  }
+
+  return REMOTE;
 }
+
